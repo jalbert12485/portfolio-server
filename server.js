b0a7e0f9b070c62d4762c3c67f398c64b5a8bd99
Justin Albert <jalbert@carthage.edu>
2020-11-09 18:58:32 -0600

    fixed typo

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const express = require("express");
 const path=require("path");
 const fs=require("fs");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
+
 // Static directory to be served
 const app = express();
 const PORT = process.env.PORT || 3000;
